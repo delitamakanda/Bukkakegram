35802a456ffea21c0df3e268d08f15a06fd25ba1
Délita Makanda <delita.makanda@gmail.com>
2017-04-01 17:27:49 +0200

    edit settings local

--- bukkake/settings.py
+++ bukkake/settings.py
@@ -140,9 +140,4 @@ STATICFILES_DIRS = (
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-# cloudinary config
-cloudinary.config(
-    cloud_name = "hctpm7uyi",
-    api_key = "817934792794216",
-    api_secret = "pK8kZUuZnp1S-jhEC9ls2yvXbS4",
-)
+from bukkake.settings_local import *
