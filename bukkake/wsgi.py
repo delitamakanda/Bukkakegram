a41771d907332538eba1b5f4d160bec97243363e
Délita Makanda <delita.makanda@gmail.com>
2017-04-01 16:26:50 +0200

    add prod files

--- bukkake/wsgi.py
+++ bukkake/wsgi.py
@@ -12,8 +12,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bukkake.settings")
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bukkake.settings_production")
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bukkake.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bukkake.settings_production")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
