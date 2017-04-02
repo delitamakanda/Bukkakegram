49e3dabeb5f5050b6400f2c429cecc2d45c820db
Délita Makanda <delita.makanda@gmail.com>
2017-04-02 21:25:03 +0200

    fix likes

--- bukkakegram/views.py
+++ bukkakegram/views.py
@@ -122,7 +122,7 @@ def like_bukkake(request):
             likes = bukkake.likes + 1
             bukkake.likes = likes
             bukkake.save()
-    messages.info(request, 'one more likes thanks !')
+            messages.info(request, 'one more likes thanks !')
     return HttpResponseRedirect(likes)
 
 def save_profile(backend, *args, **kwargs):
