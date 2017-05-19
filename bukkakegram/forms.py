d98a22e5bcefee8583c1f5aa3b1ca73e1841a845
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-05-19 11:52:14 +0200

    add captcha on signup form

--- bukkakegram/forms.py
+++ bukkakegram/forms.py
@@ -79,7 +79,8 @@ class RegisterForm(UserCreationForm):
     email = forms.EmailField(label='Email Address', max_length=255, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
     first_name = forms.CharField(label='First name', max_length=255, required=False, widget=forms.TextInput(attrs={'class': 'form-control'}))
     last_name = forms.CharField(label='Last name', max_length=255, required=False, widget=forms.TextInput(attrs={'class': 'form-control'}))
-
+    captcha = ReCaptchaField()
+    
     #def verify_password(self):
     #    if 'password1' in self.cleaned_data:
     #        password1 = self.cleaned_data['password1']
