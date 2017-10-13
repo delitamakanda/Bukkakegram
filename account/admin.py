5be9014efafc1eb41d54284d23c4a71a6cb41452
Délita Makanda <delita.makanda@gmail.com>
2017-10-14 01:43:56 +0200

    begin restart bukkake

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
-from .models import Bukkake
 
 # Register your models here.
-admin.site.register(Bukkake)
--- bukkakegram/urls.py
+++ bukkakegram/urls.py
@@ -1,35 +1,21 @@
-from django.conf.urls import url, include
-from django.conf import settings
-from django.views.static import serve
-from . import views
-from django.contrib.sitemaps.views import sitemap
-from bukkakegram.sitemap import (
-    BukkakeSitemap,
-)
+"""bukkakegram URL Configuration
 
-sitemaps = {
-    'bukkakes': BukkakeSitemap,
-}
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
 
 urlpatterns = [
-    url(r'^user/(\w+)/$', views.profile, name='profile'),
-    url(r'^$', views.index, name='index'),
-    url(r'^([0-9]+)/$', views.detail, name='detail'),
-    url(r'^post_url/$', views.post_bukkake, name="post_bukkake"),
-    url(r'^login/$', views.login_view, name="login"),
-    url(r'^logout/$', views.logout_view, name="logout"),
-    url(r'^like_bukkake/$', views.like_bukkake, name='like_bukkake'),
-    url(r'^register/$', views.register_view, name="register"),
-    url(r'^settings/$', views.settings, name='settings'),
-    url(r'^settings/password/$', views.password, name='password'),
-    url(r'^search/$', views.blog_search_list_view, name='blog_search_list_view'),
-    url('', include('social_django.urls', namespace='social')),
-    url(r'^sitemap.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    url(r'^admin/', admin.site.urls),
 ]
-
-
-if settings.DEBUG:
-    urlpatterns += [
-        url(r'^media/(?P<path>.*)$', serve,
-        {'document_root': settings.MEDIA_ROOT,}),
-    ]
