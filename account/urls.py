0eccfec8020b637605b4865d11ce22fab3d2c04d
Délita Makanda <delita.makanda@gmail.com>
2017-10-14 15:21:18 +0200

    add edit profile template and register view

--- account/urls.py
+++ account/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     #url(r'^login/$', views.user_login, name='login'),
+    url(r'^edit/$', views.edit, name='edit'),
+    url(r'^register/$', views.register, name='register'),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^logout-then-login/$', auth_views.logout_then_login, name='logout_then_login'),
