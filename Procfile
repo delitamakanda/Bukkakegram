86e6dcf7efda08903c5ced58a26009ed775c33b7
Délita Makanda <delita.makanda@gmail.com>
2017-10-15 21:14:33 +0200

    add files to prod

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: waitress-serve --port=$PORT bukkakegram.wsgi:application
---
+++ bukkakegram/settings_production.py
@@ -0,0 +1,14 @@
+#inherits from standard local settings
+from bukkakegram.settings import *
+
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['.herokuapp.com', '*',]
+
+DEBUG = config('DEBUG')
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
