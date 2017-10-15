c053477ededb2c342be43cdb1129181dbe0636b9
Délita Makanda <delita.makanda@gmail.com>
2017-10-15 23:03:09 +0200

    change url

--- account/static/scripts/bookmarklet.js
+++ account/static/scripts/bookmarklet.js
@@ -1,6 +1,6 @@
 (function() {
     var jquery_version = '2.2.4';
-    var site_url = 'http://127.0.0.1:8000/';
+    var site_url = 'https://bukkakegram.herokuapp.com/';
     var static_url = site_url + 'static/';
     var min_width = 100;
     var min_height = 100;
