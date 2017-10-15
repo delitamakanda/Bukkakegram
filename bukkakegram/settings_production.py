73c3096ddca981953cec609a11ef61033d98c58f
Délita Makanda <delita.makanda@gmail.com>
2017-10-15 23:55:22 +0200

    add media upload

--- bukkakegram/settings_production.py
+++ bukkakegram/settings_production.py
@@ -13,6 +13,7 @@ DEBUG = config('DEBUG', cast=bool)
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
+# Static files deploy in S3 AWS
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
@@ -24,3 +25,7 @@ AWS_LOCATION = 'static'
 
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# media upload files on S3 AWS
+
+DEFAULT_FILE_STORAGE = 'bukkakegram.storage_backends.MediaStorage'
