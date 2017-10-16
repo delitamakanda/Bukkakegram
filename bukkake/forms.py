a7f200a060ad63b50b8283de424cbbdf73c146f0
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-10-16 12:45:06 +0200

    Update forms.py

--- bukkake/forms.py
+++ bukkake/forms.py
@@ -15,7 +15,7 @@ class BukkakeCreateForm(forms.ModelForm):
 
     def clean_url(self):
         url = self.cleaned_data['url']
-        valid_extensions = ['jpg', 'jpeg', 'png', 'gif']
+        valid_extensions = ['jpg', 'jpeg'] # png or gif not permit due to Pillow restrictions
         extension = url.rsplit('.', 1)[1].lower()
         if extension not in valid_extensions:
             raise forms.ValidationError('The given URL does not match valid image extensions.')
