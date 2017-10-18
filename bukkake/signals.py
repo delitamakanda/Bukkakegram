2aa6ab3b926b7a65e5fa44b7797346083bd0e4b0
Délita Makanda <delitamakanda@users.noreply.github.com>
2017-10-18 13:11:11 +0200

    Update signals.py

--- bukkake/signals.py
+++ bukkake/signals.py
@@ -3,6 +3,6 @@ from django.dispatch import receiver
 from .models import Bukkake
 
 @receiver(m2m_changed, sender=Bukkake.users_like.through)
-def users_like_changed(sender, instance, **kwargs):
+def users_like_changed(sender, instance=None, **kwargs):
     instance.total_likes = instance.users_like.count()
     instance.save()
