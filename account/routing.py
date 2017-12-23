e2c609d919ef29b2784cf53b1e3fdadd7d60ae8b
Délita Makanda <delita.makanda@gmail.com>
2017-12-23 22:54:41 +0100

    add chat room model

---
+++ account/routing.py
@@ -0,0 +1,20 @@
+from channels.routing import route
+from channels import include
+from account.consumers import chat_connect, chat_disconnect, chat_receive, loadhistory_connect, loadhistory_disconnect, loadhistory_receive
+
+chat_routing = [
+    route("websocket.connect", chat_connect),
+    route("websocket.disconnect", chat_disconnect),
+    route("websocket.receive", chat_receive)
+]
+
+loadhistory_routing = [
+    route("websocket.connect", loadhistory_connect),
+    route("websocket.disconnect", loadhistory_disconnect),
+    route("websocket.receive", loadhistory_receive)
+]
+
+channel_routing = [
+    include(chat_routing, path=r"^/ws/$"),
+    include(loadhistory_routing, path=r"^/loadhistory/$"),
+]
