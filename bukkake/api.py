2a168a3231a596b4278e43ca664465f267a68845
Délita Makanda <delita.makanda@gmail.com>
2017-12-25 16:39:21 +0100

    change api

--- bukkake/api.py
+++ bukkake/api.py
@@ -1,9 +1,11 @@
 from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
 from bukkake.models import Bukkake
-from tastypie.authorization import Authorization
+from tastypie.authorization import Authorization, DjangoAuthorization
 from tastypie.authentication import BasicAuthentication
 from django.contrib.auth.models import User
-from tastypie import fields
+from tastypie import fields, utils
+from tastypie.throttle import BaseThrottle, CacheThrottle
+from tastypie.paginator import Paginator
 
 
 class UserResource(ModelResource):
@@ -11,6 +13,8 @@ class UserResource(ModelResource):
         queryset = User.objects.all()
         resource_name = 'user'
         fields = ['username', 'first_name', 'last_name', 'last_login']
+        authorization = DjangoAuthorization()
+        throttle = BaseThrottle(throttle_at=100)
         filtering = {
             'username': ALL,
         }
@@ -20,10 +24,13 @@ class BukkakeResource(ModelResource):
     user = fields.ForeignKey(UserResource, 'user')
 
     class Meta:
+        allowed_methods = ['get']
         queryset = Bukkake.objects.all()
         resource_name = 'bukkake'
         authorization = Authorization()
         authentication = BasicAuthentication()
+        throttle = CacheThrottle()
+        paginator_class = Paginator
         filtering = {
             'user': ALL_WITH_RELATIONS,
             'created': ['exact', 'lt', 'lte', 'gte', 'gt'],
