6adc6e6b4ea99771bda141eab46846db782c0ed2
Délita Makanda <delita.makanda@gmail.com>
2018-03-03 19:36:28 +0100

    fix typo

--- bukkakegram/asgi.py
+++ bukkakegram/asgi.py
@@ -2,9 +2,9 @@ import os
 import channels.asgi
 from django import settings
 
-if settings.DEBUG
+if settings.DEBUG:
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bukkakegram.settings")
 else:
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bukkakegram.settings_production")
-    
+
 channel_layer = channels.asgi.get_channel_layer()
