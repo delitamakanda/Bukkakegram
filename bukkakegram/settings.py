1e217a641288b76ac0f40d20a40c509b20bb7c1f
Délita Makanda <delita.makanda@gmail.com>
2018-03-03 19:08:56 +0100

    change settings

--- bukkakegram/settings.py
+++ bukkakegram/settings.py
@@ -131,13 +131,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
 STATIC_ROOT = 'staticfiles'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static/'),
 )
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 from django.core.urlresolvers import reverse_lazy
 
