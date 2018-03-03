b10e966598e0f248e3f98cdaea1bdbd531943c2b
Délita Makanda <delita.makanda@gmail.com>
2018-03-03 19:32:32 +0100

    fix assets serve in heroku

--- bukkakegram/asgi.py
+++ bukkakegram/asgi.py
@@ -1,5 +1,10 @@
 import os
 import channels.asgi
+from django import settings
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bukkakegram.settings")
+if settings.DEBUG
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bukkakegram.settings")
+else:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bukkakegram.settings_production")
+    
 channel_layer = channels.asgi.get_channel_layer()
