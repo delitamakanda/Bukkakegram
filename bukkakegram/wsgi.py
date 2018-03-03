325e50fbb9dbfbe8d4e61f94123d26fc8a204e81
Délita Makanda <delita.makanda@gmail.com>
2018-03-03 19:13:48 +0100

    change line

--- bukkakegram/wsgi.py
+++ bukkakegram/wsgi.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bukkakegram.settings_production")
-
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bukkakegram.settings_production")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
