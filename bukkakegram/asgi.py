2eb5cc0902d8fbda5323f54a28a5a59f0838cfe3
Délita Makanda <delita.makanda@gmail.com>
2018-03-03 19:40:35 +0100

    fix

--- bukkakegram/asgi.py
+++ bukkakegram/asgi.py
@@ -1,10 +1,6 @@
 import os
 import channels.asgi
-from django import settings
 
-if settings.DEBUG:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bukkakegram.settings")
-else:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bukkakegram.settings_production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bukkakegram.settings_production")
 
 channel_layer = channels.asgi.get_channel_layer()
