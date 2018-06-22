7174d237c862940d8c167a2ad736afa07ba46984
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-06-22 15:35:11 +0200

    Update admin.py

--- bukkake/admin.py
+++ bukkake/admin.py
@@ -3,7 +3,7 @@ from .models import Bukkake
 
 # Register your models here.
 class BukkakeAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'image', 'created']
-    list_filter = ['created']
+    list_display = ['title', 'slug', 'image', 'created', 'updated']
+    list_filter = ['created', 'updated']
 
 admin.site.register(Bukkake, BukkakeAdmin)
