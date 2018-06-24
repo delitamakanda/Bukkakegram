0bff6b8000adfcae965cace933898b95405b8d48
Délita Makanda <delita.makanda@gmail.com>
2018-06-24 22:26:32 +0200

    boto3 fait de la merde

--- bukkakegram/settings_production.py
+++ bukkakegram/settings_production.py
@@ -24,8 +24,8 @@ AWS_S3_OBJECT_PARAMETERS = {
 
 # AWS_LOCATION = 'static'
 
-# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # media upload files on S3 AWS
 
