0ac87b1118429df056679567bacb108627da01fc
Délita Makanda <delita.makanda@gmail.com>
2018-06-24 22:16:11 +0200

    whitenoise

--- bukkakegram/asgi.py
+++ bukkakegram/asgi.py
@@ -1,6 +1,9 @@
 import os
 import channels.asgi
 
+from whitenoise.django import DjangoWhiteNoise
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bukkakegram.settings_production")
 
 channel_layer = channels.asgi.get_channel_layer()
+channel_layer = DjangoWhiteNoise(channel_layer)
