d496f2c66415843a71edd59b34fdbd46429419b7
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-07-20 22:58:32 +0200

    fix literal

--- bukkakegram/settings.py
+++ bukkakegram/settings.py
@@ -66,7 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'bukkake.middleware.CorsMiddleware,
+    'bukkake.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'bukkakegram.urls'
